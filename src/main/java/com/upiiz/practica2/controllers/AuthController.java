e5cedbfe2ece03aa2361b710b21781f06769f793
EstebanJGS <guerrero.saucedo.esteban@gmail.com>
2026-04-20 22:40:41 -0600

    plantilla

--- src/main/java/com/upiiz/practica2/controllers/AuthController.java
+++
@@ -1,5 +0,0 @@
-package com.upiiz.practica2.controllers;
-
-public class AuthController {
-
-}
---
+++ src/main/java/com/upiiz/practica2/controllers/MascotasController.java
@@ -0,0 +1,30 @@
+package com.upiiz.practica2.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+@Controller
+@RequestMapping("/mascotas")
+public class MascotasController {
+
+    @GetMapping("/login")
+    public String auth() {
+        return "auth/login";
+    }
+
+    @GetMapping("/register")
+    public String register() {
+        return "auth/register";
+    }
+
+    @GetMapping("/forgot-password")
+    public String forgot_password() {
+        return "auth/forgot-password";
+    }
+    
+    
+
+}
